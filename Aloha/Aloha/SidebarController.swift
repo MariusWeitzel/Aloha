44db19ccd693374872960635f2a57657a69eabe7
Alex <crush4@rigostyle.de>
2014-12-23 22:20:49 +0100

    - Kommentare hinzugefügt - Wischgeste angepasst

--- Aloha/Aloha/SidebarController.swift
+++ Aloha/Aloha/SidebarController.swift
@@ -16,10 +16,12 @@ import UIKit
 
 class Sidebar: NSObject {
     
-    let barWidth:CGFloat = 150.0
+    
     let sidebarContainerView:UIView = UIStoryboard.SidebarViewController()!.view!
     let originView:UIView!
     
+    var barWidth:CGFloat!
+    var centerNavigationController: UINavigationController!
     var animator:UIDynamicAnimator!
     var delegate:SidebarDelegate?
     var isSidebarOpen:Bool = false
@@ -31,48 +33,49 @@ class Sidebar: NSObject {
     init(sourceView:UIView){
         super.init()
         originView = sourceView
+        barWidth = originView.frame.size.width*1/3
+        
         
         setupSidebar()
         
         animator = UIDynamicAnimator(referenceView: originView)
         
+        // Gesten
+        
         let showGestureRecognizer:UIScreenEdgePanGestureRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: "handleLeftSwipe:")
         showGestureRecognizer.edges = UIRectEdge.Left
         originView.addGestureRecognizer(showGestureRecognizer)
         
-        let hideGestureRecognizer:UIScreenEdgePanGestureRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: "handleRightSwipe:")
-        hideGestureRecognizer.edges = UIRectEdge.Right
+        let hideGestureRecognizer:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "handleRightSwipe:")
+        hideGestureRecognizer.direction = UISwipeGestureRecognizerDirection.Left
         originView.addGestureRecognizer(hideGestureRecognizer)
-        
-        
-        
     }
     
-    
+    // Sidebar erzeugen
     func setupSidebar(){
-        
         sidebarContainerView.frame = CGRectMake(-originView.frame.size.width, originView.frame.origin.y, originView.frame.size.width, originView.frame.size.height)
-        
         sidebarContainerView.backgroundColor = UIColor.clearColor()
         sidebarContainerView.clipsToBounds = false
-        
-        originView.addSubview(sidebarContainerView)
+        originView.addSubview(sidebarContainerView) // Sidebar zum Ursprungs View hinzufügen
         
     }
     
-    
+    // Wischen von Links nach Rechts
     func handleLeftSwipe(recognizer:UIScreenEdgePanGestureRecognizer){
         showSidebar(true)
         delegate?.sidebarWillOpen?()
-        
     }
     
-    func handleRightSwipe(recognizer:UIScreenEdgePanGestureRecognizer){
-        showSidebar(false)
-        delegate?.sidebarWillClose?()
+    // Wischen von Rechts nach Links (allerdings im Menü)
+    func handleRightSwipe(recognizer:UISwipeGestureRecognizer){
+        let location = recognizer.locationInView(recognizer.view) // Position der Wischgeste
+        if location.x < barWidth{ // Wischgeste muss sich innerhalb des Menüs befinden
+            showSidebar(false)
+            delegate?.sidebarWillClose?()
+        }
     }
     
-    
+    // Methode, um die Sidebar mit Animation anzuzeigen
     func showSidebar(shouldOpen:Bool){
         animator.removeAllBehaviors()
         isSidebarOpen = shouldOpen
@@ -103,9 +106,11 @@ class Sidebar: NSObject {
     
 }
 
+// Erweiterung von UIStoryboard
 private extension UIStoryboard {
     class func mainStoryboard() -> UIStoryboard { return UIStoryboard(name: "Main", bundle: NSBundle.mainBundle()) }
     
+    // Methode, um den SidebarViewController aus dem Storyboard zu holen
     class func SidebarViewController() -> UIViewController? {
         return mainStoryboard().instantiateViewControllerWithIdentifier("SidebarViewController") as? UIViewController
     }
