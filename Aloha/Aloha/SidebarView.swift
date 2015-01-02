e8f7b746b34dc9a33158515aedec0e089f30c20a
Alex <crush4@rigostyle.de>
2015-01-02 10:56:16 +0100

    Switch Test

--- Aloha/Aloha/SidebarView.swift
+++ Aloha/Aloha/SidebarView.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import UIKit
+
+class SidebarView: UIViewController {
+    
+    
+    var switchDemo=UISwitch(frame:CGRectMake(700, 100, 0, 0));
+    
+    func stateChanged(switchState: UISwitch) {
+        if switchState.on {
+            println("The Switch is On")
+        } else {
+            println("The Switch is Off")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        switchDemo.on = true
+        switchDemo.setOn(true, animated: false);
+        self.view.addSubview(switchDemo);
+        // Do any additional setup after loading the view, typically from a nib.
+        switchDemo.addTarget(self, action: Selector("stateChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
